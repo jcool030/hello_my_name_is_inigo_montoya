13692b89b13680a5c87949a27d1aaedb356c8b3f
jcool030 <jcool030@uottawa.ca>
2018-10-23 16:36:08 -0400

    Added Abstract class User and made directory for all user logic

---
+++ app/src/main/java/com/seg2105app/users/ServiceProvider.java
@@ -0,0 +1,10 @@
+package com.seg2105app.users;
+
+public class ServiceProvider extends User {
+
+    private String user;
+    private String password;
+
+    public ServiceProvider(String username, String password){ super(username, password);
+    }
+}
