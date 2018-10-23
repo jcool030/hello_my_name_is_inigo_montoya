5f3563353d2b0a90158ab02b028f972043e7afd5
jcool030 <jcool030@uottawa.ca>
2018-10-23 16:50:23 -0400

    Added Abstract class User and made directory for all user logic

---
+++ app/src/main/java/com/seg2105app/users/Administrator.java
@@ -0,0 +1,12 @@
+package com.seg2105app.users;
+
+public class Administrator extends User{
+
+
+
+    public Administrator(String username, String password){
+        super(username, password);
+    }
+
+
+}
---
+++ app/src/main/java/com/seg2105app/users/User.java
@@ -0,0 +1,13 @@
+package com.seg2105app.users;
+
+public abstract class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+
+}
