924c82e50de45cd135e3401d12c082913092d0ed
jcool030 <jcool030@uottawa.ca>
2018-11-08 12:48:10 -0500

    LogIn and SignUp work using Firebase

--- app/src/main/java/com/seg2105app/delieverable1/users/DatabaseHandler.java
+++ app/src/main/java/com/seg2105app/delieverable1/users/DatabaseHandler.java
@@ -7,89 +7,99 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Build;
 import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.seg2105app.delieverable1.activities.OpeningScreenActivity;
 import com.seg2105app.delieverable1.users.*;
 
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
+
 public final class DatabaseHandler{
 
+    private static boolean childExists;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference mRef;
+
     /////////////NESTED CLASSES//////////////////////////////////////////
-    private static class UserEntry implements BaseColumns {
-        private static final String TABLE_USERS = "users";
-        private static final String COLUMN_USERNAME = "username";
-        private static final String COLUMN_PASSWORD = "password";
+    public static class UserEntry implements BaseColumns {
+        public static final String TABLE_USERS = "users";
+        public static final String COLUMN_USERNAME = "username";
+        public static final String COLUMN_PASSWORD = "password";
     }
 
     private static class UserInfoEntry implements BaseColumns {
-        private static final String TABLE_NAME = "userInfo";
         private static final String COLUMN_FIRST_NAME = "firstname";
         private static final String COLUMN_LAST_NAME = "lastname";
         private static final String COLUMN_USER_TYPE = "type";
     }
 
-    public class UserDatabaseHelper extends SQLiteOpenHelper{
-        private static final int DATABASE_VERSION = 1;
-        private static final String DATABASE_NAME = "userInfoDB.db";
-
-        public UserDatabaseHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db){
-            db.execSQL(CREATE_USERS_TABLE);
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-            db.execSQL(DELETE_USERS_TABLE);
-            onCreate(db);
-        }
-
-    }
-
-    ///////QUERY STRINGS//////////////////////////////
-    private static final String CREATE_USERS_TABLE = "CREATE TABLE " + UserEntry.TABLE_USERS + "("
-            + UserEntry._ID + " INTEGER PRIMARY KEY," + UserEntry.COLUMN_USERNAME+" TEXT"+")";
-
-    private static final String CREATE_USER_INFO_TABLE = "CREATE TABLE " + UserInfoEntry.TABLE_NAME + "("
-            + UserInfoEntry._ID + " INTEGER PRIMARY KEY," + UserInfoEntry.COLUMN_FIRST_NAME + " TEXT,"
-            + UserInfoEntry.COLUMN_LAST_NAME + "TEXT,"+ UserInfoEntry.COLUMN_USER_TYPE+ "TEXT"+")";
-
-    private static final String DELETE_USERS_TABLE = "DROP TABLE IF EXISTS " + UserEntry.TABLE_USERS;
-
     ////////DEFAULT CONSTRUCTOR///////////////////////////////////
     public DatabaseHandler(Context context){
         FirebaseApp.initializeApp(context);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = firebaseDatabase.getReference();
     }
 
     //////METHODS//////////////////////////////////////////////////
-    public void createUserWithUsernameAndPassword(Context context, User user){
-        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-
-        ContentValues values = new ContentValues();
-        values.put(UserEntry.COLUMN_USERNAME, user.getUsername());
-        values.put(UserEntry.COLUMN_PASSWORD, user.getPassword());
-//        values.put(UserEntry.COLUMN_FIRST_NAME, user.getFirstName());
-//        values.put(UserEntry.COLUMN_LAST_NAME, user.getLastName());
-//        values.put(UserEntry.COLUMN_USER_TYPE, user.getType());getType
-
-        mDatabase.getReference(UserEntry.TABLE_USERS).child(UserEntry.COLUMN_USERNAME).setValue(user.getUsername());
-        mDatabase.getReference(UserEntry.TABLE_USERS).child(UserEntry.COLUMN_PASSWORD).setValue(user.getPassword());
-        mDatabase.getReference(UserInfoEntry.TABLE_NAME).child(UserInfoEntry.COLUMN_FIRST_NAME).setValue(user.getUsername());
-        mDatabase.getReference(UserInfoEntry.TABLE_NAME).child(UserInfoEntry.COLUMN_LAST_NAME).setValue(user.getUsername());
-        mDatabase.getReference(UserInfoEntry.TABLE_NAME).child(UserInfoEntry.COLUMN_USER_TYPE).setValue(user.getUsername());
-
+    public void createUserWithUsernameAndPassword(User user, String userID){
+        String uid = mRef.push().getKey();
+        firebaseDatabase.getReference(UserEntry.TABLE_USERS).child(uid).setValue(user);
     }
+
+    public Query getReferenceToUserTable(){
+        return firebaseDatabase.getReference(UserEntry.TABLE_USERS).orderByChild(UserEntry.COLUMN_USERNAME);
+    }
+
+    public boolean valuePresentInDatabase(DataSnapshot snapshot, String value, String columnKey){
+        boolean valueIsPresent = false;
+        for (DataSnapshot ds: snapshot.getChildren()) {
+            String retrievedValue = ds.child(columnKey).getValue(String.class);
+            Log.d(TAG, "Testing username against usernameRef: " + retrievedValue + ", " + value + ", " + retrievedValue.equalsIgnoreCase(value));
+
+
+            if (retrievedValue.equalsIgnoreCase(value)) {
+            valueIsPresent = true;
+            }
+        }
+
+        return valueIsPresent;
+    }
+
+    public User validateUsernameAndPassword(DataSnapshot snapshot, String username, String password){
+        for (DataSnapshot ds: snapshot.getChildren()) {
+            String retrievedUsername = ds.child(UserEntry.COLUMN_USERNAME).getValue(String.class);
+            String retrievedPassword = ds.child(UserEntry.COLUMN_PASSWORD).getValue(String.class);
+            Log.d(TAG, "Testing username match: " + retrievedUsername + ", " + username + ", " + retrievedUsername.equalsIgnoreCase(username));
+            Log.d(TAG, "Testing password match: " + retrievedUsername + ", " + username + ", " + retrievedUsername.equals(username));
+
+            if (retrievedUsername.equalsIgnoreCase(username) && retrievedPassword.equals(password)) {
+                String mUsername = ds.child(UserEntry.COLUMN_USERNAME).getValue(String.class);
+                String mPassword = ds.child(UserEntry.COLUMN_PASSWORD).getValue(String.class);
+                String mFirstName = ds.child(UserInfoEntry.COLUMN_FIRST_NAME).getValue(String.class);
+                String mLastName = ds.child(UserInfoEntry.COLUMN_LAST_NAME).getValue(String.class);
+                String mType = ds.child(UserInfoEntry.COLUMN_USER_TYPE).getValue(String.class);
+                UserFactory userFactory = new UserFactory();
+
+                User user = userFactory.getUser(mUsername,mPassword,mFirstName,mLastName,mType);;
+
+                return user;
+            }
+        }
+
+        return null;
+    }
+
 
 //    public void readUserFromDatabase(Context context, String username){
 //        UserDatabaseHelper udbHelper = new UserDatabaseHelper(context);
