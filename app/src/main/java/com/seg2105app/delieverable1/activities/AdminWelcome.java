c4a28ce44afe8d0dd4e1fec6274418847a21394f
Dmitry Kutin <dmitrykutin@outlook.com>
2018-11-08 18:34:37 -0500

    Added service functionality

--- app/src/main/java/com/seg2105app/delieverable1/activities/AdminWelcome.java
+++ app/src/main/java/com/seg2105app/delieverable1/activities/AdminWelcome.java
@@ -11,17 +11,19 @@ import android.widget.TextView;
 //A copy of the homeowner welcome screen class
 public class AdminWelcome extends AppCompatActivity implements View.OnClickListener {
 
-    Button signoutButton;
+    Button signoutButton, createService;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_welcome);
         signoutButton = findViewById(R.id.logOutButton);
+        createService = findViewById(R.id.newService);
         Bundle bundle = getIntent().getExtras();
         String user = bundle.getString("username");
         TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
         welcomeText.setText("Welcome, " + user + " you are logged in as Admin.");
 
+        createService.setOnClickListener(this);
         signoutButton.setOnClickListener(this);
     }
 
@@ -30,5 +32,16 @@ public class AdminWelcome extends AppCompatActivity implements View.OnClickListe
         Intent signoutIntent = new Intent(this, OpeningScreenActivity.class);
         startActivity(signoutIntent);
     }
+    public void logoutClick(View v) {
+//        Intent signoutIntent = new Intent(this, OpeningScreenActivity.class);
+//        startActivity(signoutIntent);
+    }
+    public void createServiceClick (View v){
+
+    }
+    public void manageServiceClick (View v){
+
+    }
+
 }
 
