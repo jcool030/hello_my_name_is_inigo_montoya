1e3d23c922988fe2363803d6d86a750040af4122
Dmitry Kutin <dmitrykutin@outlook.com>
2018-11-09 13:02:56 -0500

    create service

--- app/src/main/java/com/seg2105app/delieverable1/activities/AdminWelcome.java
+++ app/src/main/java/com/seg2105app/delieverable1/activities/AdminWelcome.java
@@ -6,22 +6,37 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.database.Query;
+import com.seg2105app.delieverable1.users.DatabaseHandler;
 
 //A copy of the homeowner welcome screen class
 public class AdminWelcome extends AppCompatActivity {
 
     Button signoutButton, createService;
+    ListView userlv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_welcome);
         signoutButton = findViewById(R.id.logOutButton);
         createService = findViewById(R.id.newService);
+
+        userlv = findViewById(R.id.userList);
+
         Bundle bundle = getIntent().getExtras();
         String user = bundle.getString("username");
         TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
         welcomeText.setText("Welcome, " + user + " you are logged in as Admin.");
+
+        DatabaseHandler udb = new DatabaseHandler(this);
+
+        Query query = udb.getReferenceToUserTable();
+
+
+
     }
     public void logoutClick(View v) {
         Intent signoutIntent = new Intent(this, OpeningScreenActivity.class);
@@ -32,7 +47,8 @@ public class AdminWelcome extends AppCompatActivity {
         startActivity(createServiceIntent);
     }
     public void manageServiceClick (View v){
-
+        Intent manageServiceIntent = new Intent (this, ManageServiceActivity.class);
+        startActivity(manageServiceIntent);
     }
 
 }
--- app/src/main/java/com/seg2105app/delieverable1/activities/CreateServiceActivity.java
+++ app/src/main/java/com/seg2105app/delieverable1/activities/CreateServiceActivity.java
@@ -1,19 +1,33 @@
 package com.seg2105app.delieverable1.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-import android.view.View;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import com.seg2105app.delieverable1.users.*;
 import com.seg2105app.delieverable1.users.Service;
 
-public class CreateServiceActivity extends AppCompatActivity {
+public class CreateServiceActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText getHourlyRate, getServiceName;
+    private DatabaseHandler sdbHandler;
 
-    public void createService(View view){
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_service);
         getHourlyRate = findViewById(R.id.HourlyRate);
         getServiceName = findViewById(R.id.ServiceName);
 
-        new Service( getServiceName.getText().toString().trim() , Double.parseDouble(getHourlyRate.getText().toString().trim()));
+
+    }
+
+
+
+    public void onClick(View view){
+
+        sdbHandler.createService(new Service( getServiceName.getText().toString().trim() , Double.parseDouble(getHourlyRate.getText().toString().trim())));
     }
 
 
