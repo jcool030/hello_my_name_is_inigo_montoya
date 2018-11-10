f0a684e6f5555e11ff16b16b2a907798ffbab077
Adam Rust <arust051@uottawa.ca>
2018-11-10 16:42:57 -0500

    Added validation for new service input.

--- app/src/main/java/com/seg2105app/delieverable1/activities/CreateServiceActivity.java
+++ app/src/main/java/com/seg2105app/delieverable1/activities/CreateServiceActivity.java
@@ -4,9 +4,11 @@ package com.seg2105app.delieverable1.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
+
 import com.seg2105app.delieverable1.database.*;
 
 import com.seg2105app.delieverable1.database.DatabaseHandler;
@@ -27,9 +29,28 @@ public class CreateServiceActivity extends AppCompatActivity implements View.OnC
 
 
 
-    public void onClick(View view){
-        sdbHandler.createService(new Service( getServiceName.getText().toString().trim() , Double.parseDouble(getHourlyRate.getText().toString().trim())));
-    }
+    public void onClick(View view) {
+        String serviceName = getServiceName.getText().toString().trim();
+        String hourlyRateString = getHourlyRate.getText().toString().trim();
 
+        try { //try-catch block is for checking validity of hourlyRate input
+
+            if (TextUtils.isEmpty(serviceName)) { //if serviceName is empty, prompt for input
+                Toast noServiceName = Toast.makeText(CreateServiceActivity.this, "Service name is empty.", Toast.LENGTH_LONG);
+                noServiceName.show();
+
+            }else if(TextUtils.isEmpty(hourlyRateString)){ //if hourlyRate is empty, prompt for input
+                Toast noHourlyRate = Toast.makeText(CreateServiceActivity.this, "Hourly rate is empty.", Toast.LENGTH_LONG);
+                noHourlyRate.show();
+
+            }else { //if both fields are filled out, create new service
+                sdbHandler.createService(new Service(serviceName, Double.parseDouble(hourlyRateString))); //if hourlyRate invalid, throws NumberFormatException
+            }
+
+        }catch(NumberFormatException e){ //hourly rate input is invalid (can't be parsed as a double)
+            Toast noHourlyRate = Toast.makeText(CreateServiceActivity.this, "Hourly rate input is invalid.", Toast.LENGTH_LONG);
+            noHourlyRate.show();
+        }
+    }
 
 }
