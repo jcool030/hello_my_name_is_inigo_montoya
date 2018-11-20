664472d629ac52f7cff507efd3dfd1872d12ec69
Dmitry Kutin <dmitrykutin@outlook.com>
2018-11-20 15:20:25 -0500

    finished service provider activity, added functionality

--- app/src/main/java/com/seg2105app/delieverable1/activities/ServiceProviderWelcome.java
+++ app/src/main/java/com/seg2105app/delieverable1/activities/ServiceProviderWelcome.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class ServiceProviderWelcome extends AppCompatActivity {
 
@@ -16,6 +19,8 @@ public class ServiceProviderWelcome extends AppCompatActivity {
     TextView usernameText;
     EditText phoneNum, address, companyName;
     TextInputEditText textInput;
+    RadioGroup radioGroup;
+    RadioButton lisenceTrue;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,7 +36,9 @@ public class ServiceProviderWelcome extends AppCompatActivity {
         companyName = findViewById(R.id.companyName);
 
         textInput = findViewById(R.id.textInput);
+        radioGroup = findViewById(R.id.licenseBoolean);
 
+        lisenceTrue = findViewById(R.id.Yes);
 
         Bundle bundle = getIntent().getExtras();
         String user = bundle.getString("username");
@@ -45,7 +52,31 @@ public class ServiceProviderWelcome extends AppCompatActivity {
         finish();
     }
     public void updateInfo(View  v){
+        Bundle bundle = getIntent().getExtras();
 
+        int checked = radioGroup.getCheckedRadioButtonId();
+        boolean license = false;
+
+        if (checked == -1){
+            Toast toast = Toast.makeText(getApplicationContext(), "Please Check a Radio Button", Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            license = lisenceTrue.isSelected();
+        }
+
+        String getPhoneNum = phoneNum.getText().toString().trim();
+        String getAddress= address.getText().toString().trim();
+        String getCompanyName = companyName.getText().toString().trim();
+        String description = textInput.getText().toString().trim();
+
+        bundle.putString("company", getCompanyName);
+        bundle.putString("description", description);
+        bundle.putString("address", getAddress);
+        bundle.putString("phone", getPhoneNum);
+        bundle.putBoolean("license", license);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Info Updated Successfully", Toast.LENGTH_LONG);
+        toast.show();
     }
 
 }
